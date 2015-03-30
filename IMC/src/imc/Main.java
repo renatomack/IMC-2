67d030a3a00054be3bba9e930074a8bb31a998af
41320761 <41320761@092908-P31S401.macklabs.br>
2015-03-30 19:31:17 -0300

    Arquivos copidos do arquivo da laboratório 01 e alterados conforme sugestões do netbeans.

---
+++ IMC/src/imc/Main.java
@@ -0,0 +1,15 @@
+package imc;
+
+public class Main {
+
+    //Comando principal para execução do programa
+
+    public static void main(String[] args) {
+        //Criação de um objeto ex da classe Ex2
+        Ex2 ex = new Ex2();
+        //posição da tela ao abri-la
+        ex.setLocation(400, 200);
+        //tornando a tela visível
+        ex.setVisible(true);
+    }
+}
